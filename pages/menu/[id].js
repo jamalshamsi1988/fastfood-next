2821f648582091be81c96f0841bb16554ae9d12f
Jamal Shamsi <jamal.shamsi1988@gmail.com>
2023-07-27 11:29:07 +0330

    create dynamic paths id & get data

---
+++ pages/menu/[id].js
@@ -0,0 +1,46 @@
+import { useRouter } from 'next/router';
+
+
+const Details = ({data}) => {
+    const router = useRouter();
+
+    if(router.isFallback){
+        return <h2>Loading Page Please Wite </h2>
+    }
+
+  return (
+    <div>
+      
+    </div>
+  )
+}
+
+export default Details;
+
+export async function getStaticPaths(){
+    const res = await fetch("http://localhost:4000/data");
+    const json = await res.json();
+    const data = json.slice(0,10);
+    const paths = data.map((food) => ({
+        params : {id : food.id.toString()}
+    }));
+
+    return{
+            paths,
+            fallback : true,
+    }
+};
+
+export async function getStaticProps(context){
+    const {params :{id}}=context;
+    const res = await fetch(`http://localhost:4000/data/${id}`);
+    const data = await res.json();
+
+    if(!data.id){
+        notFound : true;
+    }
+    return {
+        props : {data},
+        revalidate : 10 ,//second
+    }
+}
