0e239c32b0492f174e28a639d1018771c95f8377
Jamal Shamsi <jamal.shamsi1988@gmail.com>
2023-07-30 00:22:28 +0330

    create categories page & routs page

---
+++ pages/categories/index.js
@@ -0,0 +1,12 @@
+import React from 'react'
+import CategoriesPage from '../../components/templates/CategoriesPage'
+
+const index = () => {
+  return (
+   
+      <CategoriesPage />
+  
+  )
+}
+
+export default index
