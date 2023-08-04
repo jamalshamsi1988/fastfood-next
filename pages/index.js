bea8b717d4eca197c44e70f4f4af515957a42e7b
Jamal Shamsi <jamal.shamsi1988@gmail.com>
2023-08-04 11:49:40 +0330

    fixed some problem

--- pages/index.js
+++ pages/index.js
@@ -1,43 +1,5 @@
-import { useEffect, useState } from "react"
-
+import HomePage from './../components/templates/HomePage';
 
 export default function Home() {
-  const[todos , setTodos]=useState([]);
-  const[todo , setTodo]=useState([]);
- 
-  useEffect(()=>{
-
-    async function fetchData(){
-      const res = await fetch("/api/todos");
-      const data = await res.json();
-        setTodos(data);
-    }
-    fetchData();
-  },[])
-
-  const clickHandler = async ()=>{
-   const res = fetch("/api/todos" , {
-    method : "POST",
-    body : JSON.stringify({todo}),
-    headers : {'Content-type': 'application/json'},
-   })
-   const data = (await res).json();
-   console.log(data)
-
-  }
-
-
-  return (
-   <div >
-    <ul>
-      {
-        todos.map((todo)=> <li key={todo.id}>{todo.title}</li>)
-      }
-    </ul>
-    <div>
-      <input value={todo} onChange={e => setTodo(e.target.value)} />
-      <button onClick={clickHandler}>Create new todo</button>
-    </div>
-   </div>
-  )
+  return <HomePage />;
 }
